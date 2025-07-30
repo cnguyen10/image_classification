d5c6f248e03c9c56923f671774a1891ea64b53dc
Cuong Nguyen <cn0025@surrey.ac.uk>
2025-07-30 13:16:55 +0100

    Separate model and optimizer

--- main.py
+++ main.py
@@ -48,29 +48,35 @@ def cross_entropy_loss(model: nnx.Module, x: jax.Array, y: jax.Array) -> jax.Arr
 
 
 @nnx.jit
-def train_step(x: jax.Array, y: jax.Array, optimizer: nnx.Optimizer):
+def train_step(
+    x: jax.Array,
+    y: jax.Array,
+    model: nnx.Module,
+    optimizer: nnx.Optimizer
+) -> tuple[nnx.Module, nnx.Optimizer, jax.Array]:
     """
     """
     grad_value_fn = nnx.value_and_grad(f=cross_entropy_loss, argnums=0)
-    loss, grads = grad_value_fn(optimizer.model, x, y)
+    loss, grads = grad_value_fn(model, x, y)
 
     optimizer.update(grads=grads)
 
-    return (optimizer, loss)
+    return (model, optimizer, loss)
 
 
 def train(
     data_loader: grain.DatasetIterator,
+    model: nnx.Module,
     optimizer: nnx.Optimizer,
     cfg: DictConfig
-) -> tuple[nnx.Optimizer, jax.Array]:
+) -> tuple[nnx.Module, nnx.Optimizer, jax.Array]:
     """
     """
     # metric to track the training loss
     loss_accum = nnx.metrics.Average()
 
     # set train mode
-    optimizer.model.train()
+    model.train()
 
     for _ in tqdm(
         iterable=range(cfg.dataset.length.train // cfg.training.batch_size),
@@ -94,20 +100,19 @@ def train(
             x_mixed, y = mixup_data(x, y, key, cfg.mixup.beta.a, cfg.mixup.beta.b)
             x = jnp.astype(x_mixed, x.dtype)
 
-        optimizer, loss = train_step(x=x, y=y, optimizer=optimizer)
-            
+        model, optimizer, loss = train_step(x=x, y=y, model=model, optimizer=optimizer)
 
         if jnp.isnan(loss):
             raise ValueError('Training loss is NaN.')
 
         loss_accum.update(values=loss)
 
-    return (optimizer, loss_accum.compute())
+    return (model, optimizer, loss_accum.compute())
 
 
 def evaluate(
     data_loader: grain.DataLoader,
-    optimizer: nnx.Optimizer,
+    model: nnx.Module,
     num_samples: int,
     batch_size: int,
     progress_bar_flag: bool,
@@ -119,7 +124,7 @@ def evaluate(
     acc_accum = nnx.metrics.Accuracy()
 
     # switch to eval mode
-    optimizer.model.eval()
+    model.eval()
 
     for samples in tqdm(
         iterable=data_loader,
@@ -134,7 +139,7 @@ def evaluate(
         x = jnp.array(object=samples['image'], dtype=dtype)
         y = jnp.array(object=samples['label'], dtype=jnp.int32)
 
-        logits = optimizer.model(x)
+        logits = model(x)
 
         acc_accum.update(logits=logits, labels=y)
     
@@ -186,7 +191,7 @@ def main(cfg: DictConfig) -> None:
         dtype=eval(cfg.jax.dtype)
     )
 
-    state = nnx.Optimizer(
+    optimizer = nnx.Optimizer(
         model=model,
         tx=init_tx(
             dataset_length=len(source_train),
@@ -199,14 +204,12 @@ def main(cfg: DictConfig) -> None:
             key=random.randint(a=0, b=100)
         )
     )
-
-    del model
     # endregion
 
     # options to store models
     ckpt_options = ocp.CheckpointManagerOptions(
-        save_interval_steps=100,
-        max_to_keep=1,
+        save_interval_steps=50,
+        max_to_keep=10,
         step_format_fixed_length=3,
         enable_async_checkpointing=True
     )
@@ -230,9 +233,8 @@ def main(cfg: DictConfig) -> None:
             mlflow_run.info.run_id
         )
 
-        # enable an orbax checkpoint manager to save model's parameters
+        # enable an orbax checkpoint manager to load model's parameters
         with ocp.CheckpointManager(directory=ckpt_dir, options=ckpt_options) as ckpt_mngr:
-
             if cfg.experiment.run_id is None:
                 start_epoch_id = 0
 
@@ -251,104 +253,112 @@ def main(cfg: DictConfig) -> None:
 
                 checkpoint = ckpt_mngr.restore(
                     step=start_epoch_id,
-                    args=ocp.args.StandardRestore(item=nnx.state(state.model))
+                    args=ocp.args.StandardRestore(item=nnx.state(model))
                 )
 
-                nnx.update(state.model, checkpoint)
+                nnx.update(model, checkpoint)
 
                 del checkpoint
-            
-            # create iterative datasets as data loaders
-            dataloader_train = initialize_dataloader(
-                data_source=source_train,
-                num_epochs=cfg.training.num_epochs - start_epoch_id + 1,
-                shuffle=True,
-                seed=random.randint(a=0, b=255),
-                batch_size=cfg.training.batch_size,
-                resize=cfg.data_augmentation.resize,
-                padding_px=cfg.data_augmentation.padding_px,
-                crop_size=cfg.data_augmentation.crop_size,
-                mean=cfg.data_augmentation.mean,
-                std=cfg.data_augmentation.std,
-                p_flip=cfg.data_augmentation.prob_random_flip,
-                num_workers=cfg.data_loading.num_workers,
-                num_threads=cfg.data_loading.num_threads,
-                prefetch_size=cfg.data_loading.prefetch_size
-            )
-            dataloader_train = iter(dataloader_train)
 
-            data_loader_test_fn = partial(
-                initialize_dataloader,
-                num_epochs=1,
-                shuffle=False,
-                seed=0,
-                batch_size=cfg.training.batch_size,
-                resize=cfg.data_augmentation.crop_size,
-                padding_px=None,
-                crop_size=None,
-                mean=cfg.data_augmentation.mean,
-                std=cfg.data_augmentation.std,
-                p_flip=None,
-                is_color_img=True,
-                num_workers=cfg.data_loading.num_workers,
-                num_threads=cfg.data_loading.num_threads,
-                prefetch_size=cfg.data_loading.prefetch_size
-            )
-            dataloader_test = data_loader_test_fn(source_test)
-            dataloader_train_1 = data_loader_test_fn(source_train)
+        # create iterative datasets as data loaders
+        dataloader_train = initialize_dataloader(
+            data_source=source_train,
+            num_epochs=cfg.training.num_epochs - start_epoch_id + 1,
+            shuffle=True,
+            seed=random.randint(a=0, b=255),
+            batch_size=cfg.training.batch_size,
+            resize=cfg.data_augmentation.resize,
+            padding_px=cfg.data_augmentation.padding_px,
+            crop_size=cfg.data_augmentation.crop_size,
+            mean=cfg.data_augmentation.mean,
+            std=cfg.data_augmentation.std,
+            p_flip=cfg.data_augmentation.prob_random_flip,
+            num_workers=cfg.data_loading.num_workers,
+            num_threads=cfg.data_loading.num_threads,
+            prefetch_size=cfg.data_loading.prefetch_size
+        )
+        dataloader_train = iter(dataloader_train)
 
-            for epoch_id in tqdm(
-                iterable=range(start_epoch_id, cfg.training.num_epochs, 1),
-                desc='progress',
-                ncols=80,
-                leave=True,
-                position=1,
-                colour='green',
-                disable=not cfg.data_loading.progress_bar
-            ):
-                state, loss = train(
-                    data_loader=dataloader_train,
-                    optimizer=state,
-                    cfg=cfg
+        data_loader_test_fn = partial(
+            initialize_dataloader,
+            num_epochs=1,
+            shuffle=False,
+            seed=0,
+            batch_size=cfg.training.batch_size,
+            resize=cfg.data_augmentation.crop_size,
+            padding_px=None,
+            crop_size=None,
+            mean=cfg.data_augmentation.mean,
+            std=cfg.data_augmentation.std,
+            p_flip=None,
+            is_color_img=True,
+            num_workers=cfg.data_loading.num_workers,
+            num_threads=cfg.data_loading.num_threads,
+            prefetch_size=cfg.data_loading.prefetch_size
+        )
+        dataloader_test = data_loader_test_fn(source_test)
+        dataloader_train_1 = data_loader_test_fn(source_train)
+
+        for epoch_id in tqdm(
+            iterable=range(start_epoch_id, cfg.training.num_epochs, 1),
+            desc='progress',
+            ncols=80,
+            leave=True,
+            position=1,
+            colour='green',
+            disable=not cfg.data_loading.progress_bar
+        ):
+            model, optimizer, loss = train(
+                data_loader=dataloader_train,
+                model=model,
+                optimizer=optimizer,
+                cfg=cfg
+            )
+
+            mlflow.log_metric(
+                key='loss',
+                value=loss,
+                step=epoch_id + 1,
+                synchronous=False
+            )
+
+            if (epoch_id + 1) % cfg.training.eval_every_n_epochs == 0:
+                accuracy_train = evaluate(
+                    data_loader=dataloader_train_1,
+                    model=model,
+                    num_samples=cfg.dataset.length.train,
+                    batch_size=cfg.training.batch_size,
+                    progress_bar_flag=cfg.data_loading.progress_bar,
+                    dtype=eval(cfg.jax.dtype)
+                )
+
+                accuracy = evaluate(
+                    data_loader=dataloader_test,
+                    model=model,
+                    num_samples=cfg.dataset.length.test,
+                    batch_size=cfg.training.batch_size,
+                    progress_bar_flag=cfg.data_loading.progress_bar,
+                    dtype=eval(cfg.jax.dtype)
+                )
+
+                mlflow.log_metrics(
+                    metrics={
+                        'accuracy/test': accuracy,
+                        'accuracy/train': accuracy_train
+                    },
+                    step=epoch_id + 1,
+                    synchronous=False
+                )
+
+            with ocp.CheckpointManager(directory=ckpt_dir, options=ckpt_options) as ckpt_mngr:
+                # save parameters asynchronously
+                ckpt_mngr.save(
+                    step=epoch_id + 1,
+                    args=ocp.args.StandardSave(nnx.state(model))
                 )
 
                 # wait for checkpoint manager completing the asynchronous saving
                 ckpt_mngr.wait_until_finished()
-
-                # save parameters asynchronously
-                ckpt_mngr.save(
-                    step=epoch_id + 1,
-                    args=ocp.args.StandardSave(nnx.state(state.model))
-                )
-
-                if (epoch_id + 1) % cfg.training.eval_every_n_epochs == 0:
-                    accuracy_train = evaluate(
-                        data_loader=dataloader_train_1,
-                        optimizer=state,
-                        num_samples=cfg.dataset.length.train,
-                        batch_size=cfg.training.batch_size,
-                        progress_bar_flag=cfg.data_loading.progress_bar,
-                        dtype=eval(cfg.jax.dtype)
-                    )
-
-                    accuracy = evaluate(
-                        data_loader=dataloader_test,
-                        optimizer=state,
-                        num_samples=cfg.dataset.length.test,
-                        batch_size=cfg.training.batch_size,
-                        progress_bar_flag=cfg.data_loading.progress_bar,
-                        dtype=eval(cfg.jax.dtype)
-                    )
-
-                    mlflow.log_metrics(
-                        metrics={
-                            'loss': loss,
-                            'accuracy/test': accuracy,
-                            'accuracy/train': accuracy_train
-                        },
-                        step=epoch_id + 1,
-                        synchronous=False
-                    )
 
     return None
 
